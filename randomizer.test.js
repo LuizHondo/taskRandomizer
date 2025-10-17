1ed5209ed681bf8dc170541ef58169398066eab2
Luiz Paulo Hondo <120063146+LuizHondo@users.noreply.github.com>
2025-10-17 00:25:39 -0300

    add tests to randomizer

--- randomizer.test.js
+++ randomizer.test.js
@@ -1,3 +1,18 @@
 const randomizer = require("./randomizer");
 
-// Escreva seu código aqui
+it('should return a random number grater than the minimum',() =>{
+    const min = 10;
+
+    expect(randomizer()).toBeGreaterThan(min);
+})
+it('should return a number lesser than the maximum',()=>{
+    const max = 80
+    expect(randomizer()).toBeLessThan(max); 
+})
+it('should return a Int value',()=>{
+    expect(typeof(randomizer())).toEqual(typeof(10));
+})
+it('should return a number',()=>{
+    expect(typeof randomizer()).toBe("number");
+})
+
